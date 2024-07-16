def849124edf9bfbb198c61250670ae143efb634
Haruna Nishita <h.nishita.1@gmail.com>
2024-07-16 09:19:07 +0900

    refactor: 共通のレイアウトファイルを作成&定数用ファイルを作成

---
+++ src/utils/constants.ts
@@ -0,0 +1,27 @@
+import image2020 from "../assets/2020/ship_2020.webp";
+import image2021 from "../assets/2021/mountain_2021.webp";
+import image2022 from "../assets/2022/snow-mountain_2022.webp";
+import image2023 from "../assets/2023/flower_2023.webp";
+
+export const REPRESENTATIVE_IMAGES = [
+  {
+    img: image2020,
+    alt: "Taken photos in 2020",
+    year: "2020",
+  },
+  {
+    img: image2021,
+    alt: "Taken photos in 2021",
+    year: "2021",
+  },
+  {
+    img: image2022,
+    alt: "Taken photos in 2022",
+    year: "2022",
+  },
+  {
+    img: image2023,
+    alt: "Taken photos in 2023",
+    year: "2023",
+  },
+];
