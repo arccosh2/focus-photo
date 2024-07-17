db4cc59547243cf3d62486b6f82862a71136f341
Haruna Nishita <h.nishita.1@gmail.com>
2024-07-17 20:49:10 +0900

    update: 不要なファイル・コードを削除&スタイルの変更&titleがうまく表示されてなかったのを修正

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import image2020 from "../assets/2020/ship_2020.webp";
 import image2021 from "../assets/2021/mountain_2021.webp";
-import image2022 from "../assets/2022/snow-mountain_2022.webp";
+import image2022 from "../assets/2022/summer_2022.webp";
 import image2023 from "../assets/2023/flower_2023.webp";
 
 export const REPRESENTATIVE_IMAGES = [
