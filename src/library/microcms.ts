6ca1f0ddfa7ceb462a5f7e5d23e2c4fa2c98e451
Haruna Nishita <h.nishita.1@gmail.com>
2024-08-17 00:07:13 +0900

    update: microCMSとの疎通

---
+++ src/library/microcms.ts
@@ -0,0 +1,47 @@
+import type { MicroCMSQueries } from "microcms-js-sdk";
+import { createClient } from "microcms-js-sdk";
+
+type Tag =
+  | "mountain"
+  | "ocean"
+  | "animal"
+  | "spring"
+  | "summer"
+  | "autumn"
+  | "winter"
+  | "countryside"
+  | "trip"
+  | "other";
+
+interface Visual {
+  id: string;
+  title: string;
+  photo: {
+    url: string;
+    width: string;
+    height: string;
+  };
+  caption: string;
+  year: number;
+  tag: Tag[];
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+}
+
+interface VisualsResponse {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Visual[];
+}
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export const getVisuals = async () => {
+  return await client.get<VisualsResponse>({ endpoint: "visuals" });
+};
