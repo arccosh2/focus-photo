7dd140f30297cc3b9d69797539ae55554c9cdea7
Haruna Nishita <h.nishita.1@gmail.com>
2024-12-08 13:51:18 +0900

    add: タブに下線を追加

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import type { Visual } from "../library/microcms";
+import { useAnimate } from "framer-motion";
 import styles from "../styles/card.module.css";
 
 interface Props {
@@ -8,14 +9,33 @@ interface Props {
 
 export const Cards: React.FC<Props> = ({ visuals }) => {
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const [underBar, animate] = useAnimate();
+
+  const transformUnderBar = (index: number) => {
+    if (!underBar.current || index === activeIndex) return;
+
+    // TODO: 解像度に応じて移動量を調整する
+    const moveX = index * 170;
+
+    const moveAnimation = (moveX: number) => {
+      animate(
+        underBar.current,
+        { x: moveX },
+        { ease: "backOut", duration: 0.4 },
+      );
+    };
+
+    moveAnimation(moveX);
+  };
 
   const handleClick = (index: number) => {
+    transformUnderBar(index);
     setActiveIndex(index);
   };
 
   return (
     <div className={styles.wrapper}>
-      <div className={styles.thumbnails} data-scroll-area>
+      <div className={styles.thumbnails}>
         {visuals.map((visual, index) => (
           <button onClick={() => handleClick(index)}>
             <div
@@ -33,6 +53,7 @@ export const Cards: React.FC<Props> = ({ visuals }) => {
             </div>
           </button>
         ))}
+        <span className={styles.underBar} ref={underBar} />
       </div>
       <img
         src={visuals[activeIndex].photo.url + "?fit=crop&w=960&h=540"}
