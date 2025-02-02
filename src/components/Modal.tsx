2a040b944c34429c5bde7e114b4d76186d7f2cf3
Haruna Nishita <h.nishita.1@gmail.com>
2025-02-02 12:25:03 +0900

    update: モーダルにpandaCSSを適用

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import closeIcon from "src/assets/common/ic_close.svg";
-import styles from "src/styles/modal.module.css";
+import { css } from "styled-system/css";
 
 interface Props {
   children: React.ReactNode;
@@ -16,11 +15,65 @@ export const Modal: React.FC<Props> = ({
   if (!isOpen) return null;
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.back_drop} onClick={handleModalClose}></div>
-      <div className={styles.contents_area}>
-        <button className={styles.close_button} onClick={handleModalClose}>
-          <img src={closeIcon.src} className={styles.close_icon} />
+    <div
+      className={css({
+        position: "fixed",
+        top: "0",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        display: "grid",
+        overflowY: "auto",
+        zIndex: "modal",
+      })}
+    >
+      <div
+        className={css({
+          position: "absolute",
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "#000",
+          opacity: "0.5",
+        })}
+        onClick={handleModalClose}
+      ></div>
+      <div
+        className={css({
+          position: "absolute",
+          top: "136px",
+          right: "340px",
+          width: "320px",
+          height: "160px",
+          color: "#222222",
+          backgroundColor: "#e6e6e6",
+          borderRadius: "16px",
+          padding: "1.2%",
+          overflow: "scroll",
+          zIndex: "contents",
+        })}
+      >
+        <button
+          className={css({
+            position: "absolute",
+            top: "10px",
+            right: "10px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "32px",
+            height: "32px",
+            backgroundColor: "#c5c5c5",
+            borderRadius: "50%",
+          })}
+          onClick={handleModalClose}
+        >
+          <img
+            src={closeIcon.src}
+            className={css({
+              width: "18px",
+              height: "18px",
+            })}
+          />
         </button>
         {children}
       </div>
