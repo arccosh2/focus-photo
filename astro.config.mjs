361bd8bd4e25b18e0f6b482bc0f7227ae13b41b8
Haruna Nishita <h.nishita.1@gmail.com>
2025-02-22 18:44:13 +0900

    remove: 不要なパッケージを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: "static",
-  adapter: vercelStatic({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
   site: "https://focus-photo.vercel.app/",
 });
